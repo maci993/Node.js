b378446c4fdc78618ccec5da86ec3fac5b6a2fbe
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2023-12-14 09:47:31 +0100

    Update index.js

--- domasno1/index.js
+++ domasno1/index.js
@@ -1,17 +1,13 @@
-let numbers = [13, 40, 35, 64, 27, 52];
+let numbers = [13, 40, 35, 64, 27, 52, 43, 12];
 let oddNumbers = numbers.filter(num => num % 2 === 1);
 console.log(oddNumbers);
 
-let numbers1 = [13, 40, 35, 64, 27, 52];
-let sortNumbers = numbers1.sort((a, b) => a - b);
+
+let sortNumbers = numbers.sort((a, b) => a - b);
 console.log(sortNumbers);
 
-
-let numbers2 = [13, 40, 35, 64, 27, 52];
-let sum = numbers2.reduce((acc, curr) => acc + curr, 0);
+let sum = numbers.reduce((acc, curr) => acc + curr, 0);
 console.log(sum);
 
-
-let numbers3 = [13, 40, 35, 64, 27, 52];
-let findNumbers = numbers3.find(num => num < 33);
+let findNumbers = numbers3.find(num => num < 40 );
 console.log(findNumbers);
