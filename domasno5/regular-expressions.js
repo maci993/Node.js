957b9dfe756e0f1bdc7215b7c56271cdcde3dd8d
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2024-01-25 10:35:27 +0100

    Add files via upload

---
+++ domasno5/regular-expressions.js
@@ -0,0 +1,29 @@
+1. Random number => ^\d{3}-\d{3}$
+265-251
+
+2. username => ^[a-z]{0,5}[A-Z]{0,1}[0-9]{0,3}[!@#$%&+|.,]{0,1}$
+maciS25.
+
+3. email address => ^[a-zA-Z\d_.+-]+@[a-zA-Z\d-]+.[a-zA-Z\d-.]+$
+marina@gmail.com
+
+4. street adress => ^[a-zA-Z\d\s-/,]{5,30}$
+ulica 1615 broj 2/20 Aerodrom
+
+5. fullName => ^[A-Z][a-zA-Z]{1,20}\s[A-Z][a-zA-Z]{1,20}$
+Marina Stojanovic
+
+6. password => ^[a-z]{6}[0-9]{1}[!@#$%&*+]{1}$
+marina5!
+
+7. mobile number => ^\+[0-9]{3}/[0-9]{2}-[0-9]{3}-[0-9]{3}$
++389/77-754-050
+
+8. EMBG => ^[\d^\s]{13}
+2512993475007
+
+9. date of birth => ^\d{2}\.\d{2}\.\d{4}\.$
+25.12.1993.
+
+10. ZipCode => ^\d{4}\s[A-Z][a-z]*$
+1000 Skopje
