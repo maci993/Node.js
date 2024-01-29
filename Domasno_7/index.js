9d9680dfa05d849040dcfed625bd01252123b318
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2024-01-29 22:08:46 +0100

    Add files via upload

---
+++ Domasno_7/index.js
@@ -0,0 +1,24 @@
+// Napravete GET i POST ruta so pomos na express 
+// bibliotekata kade sto ke zemate recepti i ke 
+// dodavate
+// novi vo data.json fajlot.
+
+// Primer podatoci:
+//  [{ "recept": "Alva", "sostojki":
+//   { "jajca": 2, "mleko": 1 } }]
+
+// *Bonus: Probajte da napravite i PUT ruta.
+
+const express = require("express");
+const {
+    getRecipe,
+    addRecipe,
+} = require ("./recipes");
+
+const app = express();
+app.use(express.json());
+
+app.get("/recipes", getRecipe);
+app.post("/recipes", addRecipe);
+
+app.listen(3000, () => console.log("Server started at port 3000"));
